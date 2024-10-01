8e9fc3bbee556a5e15e11339441944ec7d4fbf44
cle4nr <144906981+cle4nr@users.noreply.github.com>
2024-10-01 13:41:50 +0100

    Add files via upload

---
+++ challenges/ch097.py
@@ -0,0 +1,8 @@
+#ch 097
+
+list2d = [[2,5,8],[3,7,4],[1,6,9],[4,2,0]]
+
+row = int(input('Enter a row: '))
+column = int(input('Enter a column: '))
+print('Your value is:',list2d[row][column])
+    
