529a6f45891a90192f98f279fea356b7861009a4
cle4nr <ronan17832@gmail.com>
2024-10-02 14:12:54 +0100

    changed index of row display

--- challenges/098 ch.py
+++ challenges/098 ch.py
@@ -1,7 +1,7 @@
 #098 ch
 list2d = [[2,5,8],[3,7,4],[1,6,9],[4,2,0]]
 rowdis = int(input('Enter the row you would like displayed: '))
-
+rowdis = rowdis - 1
 print(list2d[rowdis])
     
 addval = int(input('Enter a value to add to this row '))
