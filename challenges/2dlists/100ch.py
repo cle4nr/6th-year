9e1a31d359bab066f2f790643b167a6dd78b40c6
cle4nr <ronan17832@gmail.com>
2024-10-08 12:44:00 +0100

    ew

--- challenges/2dlists/100ch.py
+++ challenges/2dlists/100ch.py
@@ -1,2 +1,9 @@
 #ch100
-my2ddic = {'John': {'N':3056,'S':8463,'E':8441,'W':2694},'Tom': {'N':3056,'S':8463,'E':8441,'W':2694}}
+my2ddic = {'John': {'N':3056,'S':8463,'E':8441,'W':2694},'Tom': {'N':4832,'S':6786,'E':4737,'W':3612},'Anne': {'N':5239,'S':4802,'E':5820,'W':1859},'Fiona': {'N':3904,'S':4802,'E':8821,'W':2451}}
+
+print(my2ddic['John'])
+print(my2ddic['Tom'])
+print(my2ddic['Anne'])
+print(my2ddic['Fiona'])
+
+
