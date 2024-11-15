bf0feb304eb46c47ceb840f41d72f27efc4a3fe0
cle4nr <ronan17832@gmail.com>
2024-11-15 12:58:36 +0000

    biggestnums

--- js/intro to js/3intro1.js
+++ js/intro to js/3intro1.js
@@ -14,11 +14,34 @@ let car1 = {'engineSize':1200,'name':'Mazda 3'}
 canDriveCar(user1,car1)
 
 function areAllNumbersEven(numbers){
-    for (let i = 0; i < numbers.length; i++){
-    if (numbers[i] %2==0){console.log('even')}
-    }
+    let evenamt = 0
+    for (i in numbers){
+        if (numbers[i]%2==0){evenamt++}} //need to do numbers[i] because js auto uses index
+    if (evenamt == numbers.length){console.log('even')}
+    else {console.log('not all are even')}
+    
 }
 
 const nums = [2,4,6,8]
 areAllNumbersEven(nums)
 
+function getBiggestNumberInArrays(numbers1,numbers2){
+let big1 = 0
+let big2 = 0
+for (i in numbers1){
+    if (numbers1[i]>0){big1 = numbers1[i]}
+    if (numbers1[i]>big1){big1 = numbers1[i]}
+
+}
+for (i in numbers2){
+    if (numbers2[i]>0){big2 = numbers2[i]}
+    if (numbers2[i]>big2){big2 = numbers2[i]}
+
+}
+if(big1>=big2){console.log(big1)}
+else(console.log(big2))
+
+}
+const nums1 = [2,4,6,8]
+const nums2 = [2,3,8,9]
+getBiggestNumberInArrays(nums1,nums2)
